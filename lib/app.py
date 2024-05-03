d5e745afed082ee2e451d3e6fde913db0b8bcac9
bmgeier <geier.benjamin@gmail.com>
2024-05-03 13:01:15 -0400

    finished first encounter

--- lib/app.py
+++ lib/app.py
@@ -1,7 +1,10 @@
+import random
+
 class App:
 
     def __init__(self):
         self.user_input = ''
+        
 
     
     def run(self):
@@ -13,51 +16,50 @@ class App:
         print("1. Start New Game")
         print("2. About")
 
-        while self.main_user_input not in ["1", "2"]:
-            self.main_user_input = input(">>> ")
+        while self.user_input not in ["1", "2"]:
+            self.user_input = input(">>> ")
             if self.user_input not in ["1", "2"]:
                 print("That is not a valid option. Are you sure you want to be a programmer?")
-            if self.main_user_input == "1":
+            if self.user_input == "1":
                 self.start_game()
-            if self.main_user_input == "2":
+            if self.user_input == "2":
                 self.about()
 
     def about(self):
-        print("Magical Code Quest is a text-based game created by Alex, Ben and Dan. It is a CLI game created entirely in Python. We hope you enjoy it.")
+        print("Magical Code Quest is a text-based game created by Alex, Ben and Danny. It is a CLI game created entirely in Python. We hope you enjoy it.")
         print('''Start a new game?
               1. Yes
               2. No, I'm scared!
               ''')
 
-        while self.about_user_input not in ["1", "2"]:
-            self.about_user_input = input(">>> ")
+        while self.user_input not in ["1", "2"]:
+            self.user_input = input(">>> ")
             print("That is not a valid option. Are you sure you want to be a programmer?")
-            if self.about_user_input == "1":
+            if self.user_input == "1":
                 print("Excellent choice. Let the adventure begin...")
                 self.start_game()
-            if self.about_user_input == "2":
+            if self.user_input == "2":
                 print("Too bad. The hour of destiny is upon us.")
                 self.start_game()
             
 
     def start_game(self):
-        print('''
+        print("""
               The year is 2024. The world is falling apart, the job market is a mess, and you are living by yourself 
               in a one-bedroom hovel in the magical land of Eboracum Novum with four roommates, all of whom are monstrous
               trolls (literal monstrous trolls -- no looks-shaming here!)-- and your rent is still more than you can afford! 
-              ''')
-        print(''' 
+              """)
+        print("""
               Hoping to create a better life for yourself -- and perhaps find a home free of trolls -- you decide to embark on a 
               new career wielding magic -- in other words, you've signed up for Schola FlatFerrum, a coding bootcamp! You've scrounged together the 
               neccesary doubloons (adding a fifth troll roommate to make up for the cost) and you're all ready to go --
               all they need is your name.
-              ''')
+              """)
         self.user_input = input(">>> ")
-
         self.name = self.user_input
 
-        print (f'''Welcome to coding bootcamp, {self.name}! As it turns out, your old rival from childhood is also enrolled at this bootcamp.
-               Err... What's their name again?''' )
+        print (f"""Welcome to coding bootcamp, {self.name}! As it turns out, your old rival from childhood is also enrolled at this bootcamp.
+               Err... What's their name again?""" )
         
         self.user_input = input(">>> ")
         self.rival = self.user_input
@@ -66,61 +68,79 @@ class App:
         self.entrance()
 
     def entrance(self):
-
-        print ('''
+        print ("""
                As you enter your coding bootcamp, it becomes clear that something isn't right. A dark aura seems to to coat the room like a thick layer of fog.
                Most of the lights are dimmed, but you see a flicker of light coming from one room. You walk over and investigate.
-               ''')
+               """)
         self.first_encounter()
 
     def first_encounter(self):
-        print('''
+        self.question1_input = ""
+        self.question2_input = ""
+
+        print("""
               You walk into the room, which has a bloody sign reading "TURING" overhead. The room sits empty except for a single behoodied
               man sitting at  table. Behind his fashionable spectacles, there is nothing behind his glazed-over eyes. He speaks in a ghastly monotone
-              ''')
-
-        print(f'''Hello, {self.name} I am Chett, an instructor here at Schola FlatFerrum. To move on to the next room -- in our school that is,
+              """)
+        print(f"""Hello, {self.name} I am Chett, an instructor here at Schola FlatFerrum. To move on to the next room -- in our school that is,
               for some reason, set like a railroad apartment -- you must answer a question about Python. If you fail, you'll get a chance to redeem yourself
-
-        print(f'''Hello, {start_game.name} I am Chett, an instructor here at Schola FlatFerrum. To move on to the next room -- in our school that is,
-              for some reason, set like a railroad apartment, you must answer a question about Python. If you fail, you'll get a chance to redeem yourself
-
+              with a game of chance""")
+        print("""
+              Here is your first question:
+              True or false: Raccoons are very cute.
               1. True
               2. False
-              ''')
-        while self.user_input not in ["1", "2"]:
-            self.user_input = input(">>> ")
-            if self.user_input not in ["1", "2"]:
+              """)
+        while self.question1_input not in ["1", "2"]:
+            self.question1_input = input(">>> ")
+            if self.question1_input not in ["1", "2"]:
                 print("That is not a valid option. Are you sure you want to be a programmer?")
-            if self.user_input == "1":
-                print('''
+            if self.question1_input == "1":
+                print("""
                       That is correct! However, that was a pretty easy question, so I'm not going to count it. Here is your real first question:
                       What does the print() function do in Python?
-                      A) It defines a class
-                      B) It displays output on the screen 
-                      C) It imports modules
-                      D) It creates a loop
-                      ''')
-                self.user_input = input(">>> ")
-                if self.user_input not in []
+                      1) It defines a class
+                      2) It displays output on the screen 
+                      3) It imports modules
+                      4) It creates a loop
+                      """)
+                self.question2_input = input(">>> ")
+                if self.question2_input not in ["1","2","3","4"]:
+                    print("That is not a valid option. Are you sure you want to be a programmer?")
+                if self.question2_input in ["1", "3", "4"]:
+                    print("""Incorrect! You should spend more time studying python. Before you do that, though, you'll need to win against me in a 
+                          dice roll""")
+                    self.dice_game()
+                if self.question2_input == "2":
+                        print("""That is correct! You may move on to the next room.""")
+                        self.second_encounter()
                 
-            if self.user_input == "2":
-                print('''
+            if self.question1_input == "2":
+                print("""
                       Bro, this one was supposed to be a gimme. I'm not even gonna let you play a game of chance. So long, sucka!
-                      ''')
+                      """)
                 self.death()
 
+    def dice_game(self):
+        print(f"First things first, {self.name} -- you need to roll a die. May lady luck be on your side. And, since I'm feeling generous, let's say tie goes to the challenger. Enter 1 to roll")
+        user_roll = random.randint(1,6)
+        print(f"You rolled a {user_roll}, {self.name}. Now I'll roll")
+        chet_roll = random.randint(1,6)
+        if user_roll >= chet_roll:
+            print(f"You rolled a {user_roll} and I rolled a {chet_roll}. I guess you win. Good luck in the next room!")
+            self.second_encounter()
+        if chet_roll > user_roll:
+            print(f"I rolled a {chet_roll} and you rolled a {user_roll}. Tough luck, {self.name}. Time to die!")
+            self.death()
+        
     
 
     def death(self):
         print("You have died. Will you go to programmer heaven or programmer hell? These are the questions you should ask yourself")
+
+    def second_encounter(self):
+        print("testing!")
        
     
-
-              A. True
-              B. False
-              ''')
-
-
 
      
